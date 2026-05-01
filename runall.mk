e08a5c7136c9dbbc38b91e9e9532ad97b0fde522
sizeru <gaafts@gmail.com>
2026-05-01 03:41:41 -0500

    fuckass run script

--- runall.mk
+++ runall.mk
@@ -2,39 +2,67 @@ WARM_INS = 25000000
 SIM_INS = 100000000
 OUTDIR = out
 
+# TRACEDIR = traces
+TRACEDIR = /scratch/cluster/speedway/cs395t/hw1/part2/traces
 TRACES = \
-	traces/bfs.trace.gz \
-	traces/cc.trace.gz \
-	traces/astar_313B.trace.gz \
-	traces/mcf_46B.trace.gz \
-	traces/omnetpp_340B.trace.gz \
-	traces/soplex_66B.trace.gz \
-	traces/sphinx3_2520B.trace.gz \
-	traces/xalancbmk_99B.trace.gz
+	/scratch/cluster/speedway/cs395t/hw1/part2/traces/bfs.trace.gz \
+	/scratch/cluster/speedway/cs395t/hw1/part2/traces/cc.trace.gz \
+	/scratch/cluster/speedway/cs395t/hw1/part2/traces/astar_313B.trace.gz \
+	/scratch/cluster/speedway/cs395t/hw1/part2/traces/mcf_46B.trace.gz \
+	/scratch/cluster/speedway/cs395t/hw1/part2/traces/omnetpp_340B.trace.gz \
+	/scratch/cluster/speedway/cs395t/hw1/part2/traces/soplex_66B.trace.gz \
+	/scratch/cluster/speedway/cs395t/hw1/part2/traces/sphinx3_2520B.trace.gz \
+	/scratch/cluster/speedway/cs395t/hw1/part2/traces/xalancbmk_99B.trace.gz
 
-BINS = \
-  champsim_skylake \
-	champsim_skylake_l2ghb_gac_i128_g2048 \
-	champsim_skylake_l2ghb_gac_i128_g262144 \
-	champsim_skylake_l2ghb_gac_i262144_g2048 \
-	champsim_skylake_l2ghb_gac_i262144_g262144 \
-	champsim_skylake_l2ghb_gdc_i128_g2048 \
-	champsim_skylake_l2ghb_gdc_i128_g262144 \
-	champsim_skylake_l2ghb_gdc_i262144_g2048 \
-	champsim_skylake_l2ghb_gdc_i262144_g262144
+# BINS = \
+#   champsim_skylake \
+# 	champsim_skylake_l2ghb_gac_i128_g2048 \
+# 	champsim_skylake_l2ghb_gac_i128_g262144 \
+# 	champsim_skylake_l2ghb_gac_i262144_g2048 \
+# 	champsim_skylake_l2ghb_gac_i262144_g262144 \
+# 	champsim_skylake_l2ghb_gdc_i128_g2048 \
+# 	champsim_skylake_l2ghb_gdc_i128_g262144 \
+# 	champsim_skylake_l2ghb_gdc_i262144_g2048 \
+# 	champsim_skylake_l2ghb_gdc_i262144_g262144
 
-.PHONY: $(BINS)
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake)
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake_l2ghb_gac_i128_g2048)
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake_l2ghb_gac_i128_g262144)
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake_l2ghb_gac_i262144_g2048)
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake_l2ghb_gac_i262144_g262144)
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake_l2ghb_gdc_i128_g2048)
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake_l2ghb_gdc_i128_g262144 )
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake_l2ghb_gdc_i262144_g2048 )
+OUTS += $(foreach trace, \
+					    $(TRACES), \
+					 		$(OUTDIR)/$(basename $(basename $(notdir $(trace))))/champsim_skylake_l2ghb_gdc_i262144_g262144)
 
-runall: $(BINS)
 
-$(BINS):
-	for trace in $(TRACES); do \
-	  name="$(basename $(basename $(notdir traces/bfs.trace.gz)))"; \
-		mkdir -p $(OUTDIR)/$$name; \
-	  ./bin/$@ \
-			--warmup-instructions ${WARM_INS} \
-			--simulation-instructions ${SIM_INS} \
-			--json $(OUTDIR)/$@.json \
-			$$trace \
-			2>&1 | tee $(OUTDIR)/@.txt; \
-	done
+.PHONY: $(OUTS)
+
+runall: $(OUTS)
+
+$(OUTS):
+	mkdir -p $(dir $@)
+	./bin/$(notdir $@) \
+		--warmup-instructions ${WARM_INS} \
+		--simulation-instructions ${SIM_INS} \
+		--json $@.json \
+		$(TRACEDIR)/$(word 2,$(subst /, ,$@)).trace.gz \
+		2>&1 | tee $@.txt;
